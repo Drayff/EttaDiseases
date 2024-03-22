838e367147ddf2ca23b13fb7704f9c8c8b627424
Drayff <drayf.funton@gmail.com>
2024-03-22 22:15:50 +0200

    Setup

---
+++ src/main/java/online/ettarp/ettadiseases/EttaDiseases.java
@@ -0,0 +1,17 @@
+package online.ettarp.ettadiseases;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EttaDiseases extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
